458a5925e9d2d88668c4db5a8d9a641187a57e99
abdi majiid haji <najacade23456@gmail.com>
2025-07-14 12:11:22 +0300

    changed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 30s linear infinite',
+      },
+    },
+  },
+}
